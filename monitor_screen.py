d38eafdf8b1416b7d5a2301a850bf06f2f0e78a8
Spiros Dimoulis <sdimoulis@gmail.com>
2024-06-05 23:54:21 +0300

    Initial commit

--- monitor_screen.py
+++ monitor_screen.py
@@ -3,39 +3,83 @@ import numpy as np
 import pyautogui
 import tkinter as tk
 from tkinter import messagebox
+import os
+import ctypes
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 import pystray
 from PIL import Image
 import threading
 import time
-import os
 import variables as var
-
-# Initialize Pygame Mixer
-pygame.mixer.init()
 
 # Path to the MP3 file to play
 mp3_file_path = var.mp3_file_path
 
+overlay_root = None
+overlay_visible = False
+
+# Create a hidden main window for Tkinter operations
+main_thread_tk = tk.Tk()
+main_thread_tk.withdraw()
+
+# Constants for showing/hiding the console window
+SW_HIDE = 0
+SW_SHOW = 5
+
+# Get a handle to the console window
+console_window_handle = ctypes.windll.kernel32.GetConsoleWindow()
+
 
 def create_overlay(region):
     """Create a transparent overlay window with a green border around the specified region."""
-    root = tk.Tk()
-    root.overrideredirect(True)  # Remove window border and controls
-    root.geometry(
+    global overlay_root
+    overlay_root = tk.Tk()
+    overlay_root.overrideredirect(True)  # Remove window border and controls
+    overlay_root.geometry(
         f"{region[2]}x{region[3]}+{region[0]}+{region[1]}"
     )  # Match the size and position
-    root.lift()
-    root.wm_attributes("-topmost", True)
-    root.wm_attributes("-transparentcolor", "blue")
+    overlay_root.lift()
+    overlay_root.wm_attributes("-topmost", True)
+    overlay_root.wm_attributes("-transparentcolor", "blue")
 
-    canvas = tk.Canvas(root, bg="blue", highlightthickness=0, bd=0)
+    canvas = tk.Canvas(overlay_root, bg="blue", highlightthickness=0, bd=0)
     canvas.pack(fill=tk.BOTH, expand=True)
     canvas.create_rectangle(
         1, 1, region[2] - 1, region[3] - 1, outline="green", width=2
     )
 
-    root.mainloop()
+    overlay_root.mainloop()
+
+
+def toggle_overlay():
+    """Toggle the visibility of the overlay window."""
+    global overlay_visible
+    if overlay_visible:
+        hide_overlay()
+    else:
+        show_overlay()
+
+
+def show_overlay():
+    """Show the overlay window."""
+    global overlay_visible
+    if not overlay_visible:
+        region = (
+            var.region
+        )  # Specify the screen region to capture (x, y, width, height)
+        threading.Thread(target=create_overlay, args=(region,), daemon=True).start()
+        overlay_visible = True
+
+
+def hide_overlay():
+    """Hide the overlay window."""
+    global overlay_visible, overlay_root
+    if overlay_visible and overlay_root:
+        overlay_root.destroy()
+        overlay_root = None
+        overlay_visible = False
 
 
 def compare_images(img1, img2):
@@ -50,23 +94,46 @@ def compare_images(img1, img2):
 def alert():
     """Play an alert sound and show a popup message."""
     # Play an MP3 file
+    pygame.init()
     pygame.mixer.music.load(mp3_file_path)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():  # Wait for the music to finish playing
         pygame.time.Clock().tick(10)
+    pygame.quit()
+    # Schedule the messagebox to be shown in the main thread
+    # main_thread_tk.after(0, show_messagebox)
 
-    # Popup message
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    messagebox.showinfo("Προσοχή", "Έχεις νέα μηνύματα για ενέργεια!")
-    root.destroy()
+
+def show_messagebox():
+    """Show a popup message on top of all other windows."""
+    top = tk.Toplevel(main_thread_tk)
+    top.withdraw()  # Hide the window
+    top.attributes("-topmost", True)  # Ensure it is on top
+    messagebox.showinfo("Προσοχή", "Έχεις νέα μηνύματα για ενέργεια!", parent=top)
+    top.destroy()
+
+
+def toggle_console_window():
+    """Toggle the visibility of the console window."""
+    if ctypes.windll.user32.IsWindowVisible(console_window_handle):
+        ctypes.windll.user32.ShowWindow(console_window_handle, SW_HIDE)
+    else:
+        ctypes.windll.user32.ShowWindow(console_window_handle, SW_SHOW)
 
 
 def create_system_tray_icon():
     """Create system tray icon and menu."""
     try:
         icon_image = Image.open(var.icon_path)
-        menu = (pystray.MenuItem("Έξοδος", exit_program),)
+        menu = pystray.Menu(
+            pystray.MenuItem(
+                "Εμφάνιση/Απόκρυψη Περιοχής Ελέγχου", lambda: toggle_overlay()
+            ),
+            pystray.MenuItem(
+                "Εμφάνιση/Απόκρυψη Παραθύρου Κονσόλας", lambda: toggle_console_window()
+            ),
+            pystray.MenuItem("Έξοδος", exit_program),
+        )
         icon = pystray.Icon(
             "monitor_screen", icon_image, "Έλεγχος Μηνυμάτων Operator", menu
         )
@@ -89,13 +156,11 @@ def monitor_screen():
     # Path to save the reference image
     reference_image_path = var.reference_image_path
 
-    # Create and display the overlay in a non-blocking way
-    # threading.Thread(target=create_overlay, args=(region,), daemon=True).start()
-
     # Create and save the initial reference image
     screenshot = pyautogui.screenshot(region=region)
     reference_image = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
     cv2.imwrite(reference_image_path, reference_image)
+    # reference_image = cv2.imread(reference_image_path)
 
     while True:
         # Capture the specified region of the screen
@@ -116,8 +181,11 @@ def monitor_screen():
 
 if __name__ == "__main__":
     # Begin the screen monitoring
+    # print("Script path: ", var.script_dir)
+    print("Monitoring", var.region)
     email_thread = threading.Thread(target=monitor_screen)
     email_thread.daemon = True
     email_thread.start()
 
     create_system_tray_icon()
+    main_thread_tk.mainloop()  # Start the Tkinter main loop
